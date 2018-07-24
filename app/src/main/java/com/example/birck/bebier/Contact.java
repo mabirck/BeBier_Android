e1730fc4d280cec078a815e0a3c038b4a94dd78d
Marco Birck <mafbirck@gmail.com>
2018-07-23 22:22:16 -0300

    Simple list filter

---
+++ app/src/main/java/com/example/birck/bebier/Contact.java
@@ -0,0 +1,27 @@
+package com.example.birck.bebier;
+
+/**
+ * Created by birck on 22/07/18.
+ */
+
+
+public class Contact {
+    String name;
+    String image;
+    String phone;
+
+    public Contact() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+}
